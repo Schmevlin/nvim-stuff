54cb4cb3cc941fefb1006870ae5103e27aa547fd
Ubuntu <ubuntu@ip-172-31-30-31.ec2.internal>
2026-02-16 21:31:28 +0000

    uhhh

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,14 +2,39 @@ return {
 	{ "neovim/nvim-lspconfig" },
 	{
 		"mason-org/mason.nvim",
-		opts = {}
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
+			}
+		}
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = {},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",        -- Lua
+				"basedpyright",       -- Python
+				"ts_ls",         -- TypeScript/JavaScript
+				"rust_analyzer", -- Rust
+				"html",          -- HTML
+				"cssls",         -- CSS
+				"jsonls",        -- JSON
+				"yamlls",        -- YAML
+				"bashls",        -- Bash
+				"tailwindcss",   -- Tailwind CSS
+				"emmet_ls",      -- Emmet
+				"dockerls",      -- Docker
+				"marksman",      -- Markdown
+			},
+			-- Auto-install servers when opening relevant files
+			automatic_installation = true,},
+			dependencies = {
+				{ "mason-org/mason.nvim", opts = {} },
+				"neovim/nvim-lspconfig",
+			},
+		}
 	}
-}
