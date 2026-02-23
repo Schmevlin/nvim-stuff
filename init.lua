bcc562614be4cbf3a57b09efe021289d1fa89d11
Ubuntu <ubuntu@ip-172-31-30-31.ec2.internal>
2026-02-23 03:40:00 +0000

    made python lsp better and made harpoon normalller

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
